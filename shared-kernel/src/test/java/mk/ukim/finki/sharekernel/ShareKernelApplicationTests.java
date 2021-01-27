21d6279c4fb4382cd44d9ddfe836214c33958855
PavelJankoski <paveljankoski@hotmail.com>
2021-01-27 18:55:34 +0100

    Project setup

---
+++ shared-kernel/src/test/java/mk/ukim/finki/sharekernel/ShareKernelApplicationTests.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.sharekernel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShareKernelApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ theater-management/src/main/java/mk/ukim/finki/theatermanagement/TheaterManagementApplication.java
@@ -0,0 +1,13 @@
+package mk.ukim.finki.theatermanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheaterManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheaterManagementApplication.class, args);
+    }
+
+}
